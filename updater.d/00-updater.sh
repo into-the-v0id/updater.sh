9c0d8c2e51b2b692668d7c506ab88406ac9c5906
Void <57257404+into-the-v0id@users.noreply.github.com>
2023-07-16 11:16:36 +0200

    Fix configuration change notification of self updater

--- updater.d/00-updater.sh
+++ updater.d/00-updater.sh
@@ -4,7 +4,7 @@ function update {
     local src_dir=""
     src_dir="$(realpath "$(dirname "$(realpath "$0")")")" || return $?
 
-    local old_updaters="$src_dir/updater.d"/*.sh
+    local old_updaters=("$src_dir/updater.d"/*.sh)
 
     local src_owner=""
     src_owner="$(find "$src_dir" -maxdepth 0 -type d -printf "%U")" || return $?
@@ -14,9 +14,9 @@ function update {
         sudo git -C "$src_dir" pull || return $?
     fi
 
-    local new_updaters="$src_dir/updater.d"/*.sh
+    local new_updaters=("$src_dir/updater.d"/*.sh)
 
-    if test "$old_updaters" != "$new_updaters"; then
+    if test "${old_updaters[*]}" != "${new_updaters[*]}"; then
         echo ""
         echo "Some updaters have changed. You may need to review your configuration."
     fi
